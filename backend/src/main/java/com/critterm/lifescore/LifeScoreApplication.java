81847766702cfb9f6ca00c473b7f163783e250cd
critterm <chrismorse301@gmail.com>
2025-05-19 02:09:08 -0400

    0.0.1

---
+++ backend/src/main/java/com/critterm/lifescore/LifeScoreApplication.java
@@ -0,0 +1,13 @@
+package com.critterm.lifescore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeScoreApplication.class, args);
+	}
+
+}
