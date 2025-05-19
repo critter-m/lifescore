e90df8fd559a444958ac33c3a4e13db28f6eb05b
critterm <chrismorse301@gmail.com>
2025-05-19 02:01:43 -0400

    0.0

---
+++ src/test/java/com/critterm/lifescore/LifeScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.critterm.lifescore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
